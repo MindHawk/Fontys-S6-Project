b23b7824446aede493d46ba960cefcd54aa29607
Tim Gooren <384376@student.fontys.nl>
2022-03-28 13:53:36 +0200

    Add nUnit test scaffolds

---
+++ backend/BackendTest/RoomControllerTest.cs
@@ -0,0 +1,17 @@
+using NUnit.Framework;
+
+namespace BackendTest;
+
+public class RoomControllerTest
+{
+    [SetUp]
+    public void Setup()
+    {
+    }
+
+    [Test]
+    public void Test1()
+    {
+        Assert.Pass();
+    }
+}
